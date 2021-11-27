fa40c867b9724d84a64ac7552f8b41962c331011
Bruno Sorban <brunosorban@usp.br>
2021-11-27 15:59:12 -0300

    Modelo de suspensão com graus de liberdade nos pneus completo

--- carpy/Vehicle.py
+++ carpy/Vehicle.py
@@ -37,19 +37,37 @@ class Vehicle:
         else: return print('Please insert a valid position. Position must be "f" or "r"')
         return self.set_suspension(self.K_sf, self.K_sr, self.C_sf, self.C_sr)
 
-    def get_vertical_load(self, z, vz, phi, theta, phi_dot, theta_dot):
+    def get_vertical_load(self, z, vz, phi, theta, phi_dot, theta_dot, z1, z2, z3, z4, vz1, vz2, vz3, vz4, zc1, zc2, zc3, zc4, vzc1, vzc2, vzc3, vzc4):
+        # Retrieve suspension properties
+        Kt = self.Tire.cz
+        Ct = 0
+        K_sf = self.K_sf
+        K_sr = self.K_sr
+        C_sf = self.C_sf
+        C_sr = self.C_sr
+        
+        # Get static forces distribution
         F10 = F20 = self.Ff0
         F30 = F40 = self.Fr0
+        
+        # Calculate sines used
         sphi = np.sin(phi)
         stheta = np.sin(theta)
         sphidot = np.sin(phi_dot)
         sthetadot = np.sin(theta_dot)
 
-        F1 = F10 + self.K_sf * (-z - self.wf/2 * sphi + self.lf * stheta) + self.C_sf * (-vz - self.wf/2 * sphidot + self.lf * sthetadot)
-        F2 = F20 + self.K_sf * (-z + self.wf/2 * sphi + self.lf * stheta) + self.C_sf * (-vz + self.wf/2 * sphidot + self.lf * sthetadot)
-        F3 = F30 + self.K_sr * (-z - self.wr/2 * sphi - self.lr * stheta) + self.C_sr * (-vz - self.wr/2 * sphidot - self.lr * sthetadot)
-        F4 = F40 + self.K_sr * (-z + self.wr/2 * sphi - self.lr * stheta) + self.C_sr * (-vz + self.wr/2 * sphidot - self.lr * sthetadot)
-        return F1, F2, F3, F4
+        # Calculate tire forces        
+        Ft1 = F10 + Kt * (zc1 - z1) + Ct * (vzc1 - vz1)
+        Ft2 = F20 + Kt * (zc2 - z2) + Ct * (vzc2 - vz2)
+        Ft3 = F30 + Kt * (zc3 - z3) + Ct * (vzc3 - vz3)
+        Ft4 = F40 + Kt * (zc4 - z4) + Ct * (vzc4 - vz4)
+        
+        # Calculate suspension forces
+        Fs1 = F10 + K_sf * (z1 - z - self.wf/2 * sphi + self.lf * stheta) + C_sf * (vz1 - vz - self.wf/2 * sphidot + self.lf * sthetadot)
+        Fs2 = F20 + K_sf * (z2 - z + self.wf/2 * sphi + self.lf * stheta) + C_sf * (vz2 - vz + self.wf/2 * sphidot + self.lf * sthetadot)
+        Fs3 = F30 + K_sr * (z3 - z - self.wr/2 * sphi - self.lr * stheta) + C_sr * (vz3 - vz - self.wr/2 * sphidot - self.lr * sthetadot)
+        Fs4 = F40 + K_sr * (z4 - z + self.wr/2 * sphi - self.lr * stheta) + C_sr * (vz4 - vz + self.wr/2 * sphidot - self.lr * sthetadot)
+        return Ft1, Ft2, Ft3, Ft4, Fs1, Fs2, Fs3, Fs4
 
     def get_steer_wbw(self, delta_sw):
         # return the ackerman geometry wheel by wheel
