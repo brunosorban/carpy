DLC fix with polynom and fix in suspension force calculation

carpy/Driver.py
@@ -127,31 +127,109 @@ class Driver:
         elif steering == "DLC":
             print("Double Lane-Change defined.")
 
-            def brute_DLC(t, angle=6):
+            ## Senos ajustados
+            # def brute_DLC(t, angle=6):
+            #     if t <= 30:
+            #         return 0
+            #     elif 30 < t <= 31.93:
+            #         return np.deg2rad(angle) * np.sin((t - 30) * 2 * np.pi / 2)
+            #     elif 31.93 < t < 33.4:
+            #         return 0
+            #     elif 33.4 < t <= 35.318:
+            #         return -np.deg2rad(angle) * np.sin((t - 33.4) * 2 * np.pi / 2)
+            #     else:
+            #         return 0
+                
+            ## Senos completos
+            # def brute_DLC(t, angle=6):
+            #     if t <= 30:
+            #         return 0
+            #     elif 30 < t < 32:
+            #         return np.deg2rad(angle) * np.sin((t - 30) * 2 * np.pi / 2)
+            #     elif 32 <= t <= 34:
+            #         return 0
+            #     elif 34 < t < 36:
+            #         return -np.deg2rad(angle) * np.sin((t - 34) * 2 * np.pi / 2)
+            #     else:
+            #         return 0
+            
+            # # Polinomio de 4 grau
+            # t1 = 0.5
+            # t2 = 1
+            # angle = np.deg2rad(6)
+            # A_pol = [[t1**4, t1**3, t1**2], [t2**4, t2**3, t2**2], [4*t1**3, 3*t1**2, 2*t1]]
+            # b_pol = [angle, 0, 0]
+            # a0, a1, a2 = np.linalg.solve(A_pol, b_pol)
+            # a3 = a4 = 0
+            # f = lambda x: a0*x**4 + a1*x**3 + a2*x**2 + a3*x + a4
+            # def dlc_func(t):
+            #     if t <= 30:
+            #         return 0
+            #     elif 30 < t <= 31:
+            #         return f(t-30)
+            #     elif 31 < t <= 32:
+            #         return -f(32 - t)
+            #     elif 32 < t <= 34:
+            #         return 0
+            #     elif 34 < t <= 35:
+            #         return -f(t-34)
+            #     elif 35 < t <= 36:
+            #         return f(36 - t)
+            #     else:
+            #         return 0
+            
+            # Polinomio do 8 grau
+            t0 = 0
+            t1 = 0.5
+            t2 = 1
+            t3 = 1.5
+            t4 = 2
+            alpha = np.deg2rad(6)
+
+            A_pol = [
+                [t0**8, t0**7, t0**6, t0**5, t0**4, t0**3, t0**2, t0, 1], 
+                [t1**8, t1**7, t1**6, t1**5, t1**4, t1**3, t1**2, t1, 1], 
+                [t2**8, t2**7, t2**6, t2**5, t2**4, t2**3, t2**2, t2, 1], 
+                [t3**8, t3**7, t3**6, t3**5, t3**4, t3**3, t3**2, t3, 1], 
+                [t4**8, t4**7, t4**6, t4**5, t4**4, t4**3, t4**2, t4, 1],
+                [8 * t0**7, 7 * t0**6, 6 * t0**5, 5 * t0**4, 4 * t0**3, 3 * t0**2, 2 * t0, 1, 0],
+                [8 * t1**7, 7 * t1**6, 6 * t1**5, 5 * t1**4, 4 * t1**3, 3 * t1**2, 2 * t1, 1, 0],
+                [8 * t3**7, 7 * t3**6, 6 * t3**5, 5 * t3**4, 4 * t3**3, 3 * t3**2, 2 * t3, 1, 0],
+                [8 * t4**7, 7 * t4**6, 6 * t4**5, 5 * t4**4, 4 * t4**3, 3 * t4**2, 2 * t4, 1, 0],]
+
+            b_pol = [0, alpha, 0, -alpha, 0, 0, 0, 0, 0]
+
+            a0, a1, a2, a3, a4, a5, a6, a7, a8 = np.linalg.solve(A_pol, b_pol)
+            f = lambda x: a0 * x**8 + a1 * x**7 + a2 * x**6 + a3 * x**5 + a4 * x**4 + a5 * x**3 + a6 * x**2 + a7 * x + a8
+            
+            def dlc_func(t):
                 if t <= 30:
                     return 0
-                elif 30 < t <= 31.93:
-                    return np.deg2rad(angle) * np.sin((t - 30) * 2 * np.pi / 2)
-                elif 31.93 < t < 33.4:
+                elif 30 < t <= 32:
+                    return f(t-30)
+                elif 32 < t <= 34:
                     return 0
-                elif 33.4 < t <= 35.318:
-                    return -np.deg2rad(angle) * np.sin((t - 33.4) * 2 * np.pi / 2)
+                elif 34 < t <= 36:
+                    return -f(t-34)
                 else:
                     return 0
 
-            t = np.linspace(0, 100, 10001)
-            y = [brute_DLC(ti) for ti in t]
-            self.DLC_func = Function(
-                t,
-                y,
-                method="cubicSpline",
-                xlabel="Time (s)",
-                ylabel="Steering angle (rad)",
-            )
+            self.DLC_func = dlc_func
+            
+            # # Senos
+            # t = np.linspace(0, 100, 10001)
+            # y = [brute_DLC(ti) for ti in t]
+            # self.DLC_func = Function(
+            #     t,
+            #     y,
+            #     method="linear",
+            #     xlabel="Time (s)",
+            #     ylabel="Steering angle (rad)",
+            # )
 
             self.delta_control = self.DLC
 
-            self.DLC_func.plot2D("Steering angle in time", lower=25, upper=40)
+            # self.DLC_func.plot2D("Steering angle in time", lower=25, upper=40)
 
         elif steering == "sinusoidal":
             print("sinusoidal defined.")

carpy/Race.py
@@ -8,8 +8,9 @@ import numpy as np
 from Function import Function
 from scipy import integrate
 import matplotlib.pyplot as plt
-import sys, pygame # For the animation function
-import imageio # For the animation function
+import sys, pygame  # For the animation function
+import imageio  # For the animation function
+
 
 class Race:
     """
@@ -207,6 +208,7 @@ class Race:
             Steer angle in tire 4 Z axis. Tire 4 refers to the rear right 
             tire.
     """
+
     def __init__(
         self,
         Vehicle,
@@ -285,11 +287,27 @@ class Race:
 
         # Get track variables; A base rotation is applied to consider distance from CG to each wheel
         Ct = self.rotation_matrix(0, 0, psi)
-        [xc1, yc1, zc1] = np.transpose(Ct) @ [self.Vehicle.lf, self.Vehicle.wf, 0] + [x, y, 0]
-        [xc2, yc2, zc2] = np.transpose(Ct) @ [self.Vehicle.lf, -self.Vehicle.wf, 0] + [x, y, 0]
-        [xc3, yc3, zc3] = np.transpose(Ct) @ [-self.Vehicle.lr, self.Vehicle.wr, 0] + [x, y, 0]
-        [xc4, yc4, zc4] = np.transpose(Ct) @ [-self.Vehicle.lr, -self.Vehicle.wr, 0] + [x, y, 0]
-        
+        [xc1, yc1, zc1] = np.transpose(Ct) @ [self.Vehicle.lf, self.Vehicle.wf, 0] + [
+            x,
+            y,
+            0,
+        ]
+        [xc2, yc2, zc2] = np.transpose(Ct) @ [self.Vehicle.lf, -self.Vehicle.wf, 0] + [
+            x,
+            y,
+            0,
+        ]
+        [xc3, yc3, zc3] = np.transpose(Ct) @ [-self.Vehicle.lr, self.Vehicle.wr, 0] + [
+            x,
+            y,
+            0,
+        ]
+        [xc4, yc4, zc4] = np.transpose(Ct) @ [-self.Vehicle.lr, -self.Vehicle.wr, 0] + [
+            x,
+            y,
+            0,
+        ]
+
         zc1 = self.Track.get_track_height(xc1, yc1)
         zc2 = self.Track.get_track_height(xc2, yc2)
         zc3 = self.Track.get_track_height(xc3, yc3)
@@ -319,6 +337,8 @@ class Race:
             fs_2,
             fs_3,
             fs_4,
+            far_f,
+            far_r,
         ) = self.Vehicle.get_vertical_load(
             z,
             vvz,
@@ -343,7 +363,7 @@ class Race:
             vzc3,
             vzc4,
         )
-        
+
         if min(ftz_1, ftz_2, ftz_3, ftz_4, fs_1, fs_2, fs_3, fs_4) < 0:
             self.solver.status = "finished"
             return [
@@ -467,10 +487,10 @@ class Race:
         [ax, ay, az] = np.transpose(C) @ [avx, avy, avz]
 
         # Calculate tires vertical acceleration
-        az1 = (ftz_1 - fs_1) / self.Vehicle.Tire.mass
-        az2 = (ftz_2 - fs_2) / self.Vehicle.Tire.mass
-        az3 = (ftz_3 - fs_3) / self.Vehicle.Tire.mass
-        az4 = (ftz_4 - fs_4) / self.Vehicle.Tire.mass
+        az1 = (ftz_1 - fs_1 + far_f) / self.Vehicle.Tire.mass
+        az2 = (ftz_2 - fs_2 - far_f) / self.Vehicle.Tire.mass
+        az3 = (ftz_3 - fs_3 + far_r) / self.Vehicle.Tire.mass
+        az4 = (ftz_4 - fs_4 - far_r) / self.Vehicle.Tire.mass
 
         # phi angular acceleration
         tau_x = (
@@ -1381,7 +1401,7 @@ class Race:
         position_side = np.array([0, 128 + 52])
         position_front = np.array([0, 270])
         # last_position = position
-        timeCount = 50
+        timeCount = 0
         scale = 3
 
         # Iteration in time

carpy/Vehicle.py
@@ -1,5 +1,4 @@
 import numpy as np
-from Function import Function
 
 
 class Vehicle:
@@ -36,18 +35,22 @@ class Vehicle:
         self.Ixx = Ixx
         self.Iyy = Iyy
         self.Izz = Izz
-        # self.K_arf = 0
-        # self.K_arr = 0
+        self.K_arf = 0  # anti-roll bar parameters
+        self.K_arr = 0  # anti-roll bar parameters
         self.gamma1 = 0
         self.gamma2 = 0
         self.gamma3 = 0
         self.gamma4 = 0
         self.gravity = 9.81
-        
+
         # Pre-calculations
         self.h = CG_height - Tire.tire_radius
-        self.Ff0 = self.lr / (2 * (self.lf + self.lr) ) * self.vehicle_mass * self.gravity
-        self.Fr0 = self.lf / (2 * (self.lf + self.lr) ) * self.vehicle_mass * self.gravity
+        self.Ff0 = (
+            self.lr / (2 * (self.lf + self.lr)) * self.vehicle_mass * self.gravity
+        )
+        self.Fr0 = (
+            self.lf / (2 * (self.lf + self.lr)) * self.vehicle_mass * self.gravity
+        )
 
     def set_suspension(self, K_sf, K_sr, C_sf, C_sr):
         self.K_sf = K_sf
@@ -56,15 +59,15 @@ class Vehicle:
         self.C_sr = C_sr
 
     def set_anti_roll_bar(self, position, d=0, a=0, b=0, G=0, K_arz=False):
-        K_arz = G * (np.pi * d**4 / 32) / (b * a**2) if not K_arz else K_arz
+        K_arz = G * (np.pi * d ** 4 / 32) / (b * a ** 2) if not K_arz else K_arz
 
-        if position == 'f': 
-            self.K_arzf = K_arz
-            print('Anti-roll Bar (front) = {:.1f} Nm/৹'.format(np.deg2rad(K_arz)))
-        elif position == 'r': 
-            self.K_arzr = K_arz
-            print('Anti-roll Bar (rear)  = {:.1f} Nm/৹'.format(np.deg2rad(K_arz)))
-        else: 
+        if position == "f":
+            self.K_arf = K_arz
+            print("Anti-roll Bar (front) = {:.1f} Nm/৹".format(np.deg2rad(K_arz)))
+        elif position == "r":
+            self.K_arr = K_arz
+            print("Anti-roll Bar (rear)  = {:.1f} Nm/৹".format(np.deg2rad(K_arz)))
+        else:
             return print('Please insert a valid position. Position must be "f" or "r"')
 
         return K_arz
@@ -102,11 +105,7 @@ class Vehicle:
     ):
         # Retrieve suspension properties
         Kt = self.Tire.cz
-        Ct = 0 # pneu sem amortecimento
-        K_sf = self.K_sf
-        K_sr = self.K_sr
-        C_sf = self.C_sf
-        C_sr = self.C_sr
+        Ct = 0  # tire without damping
 
         # Get static forces distribution
         F10 = F20 = self.Ff0
@@ -115,57 +114,46 @@ class Vehicle:
         # Calculate sines used
         sphi = np.sin(phi)
         stheta = np.sin(theta)
-        sphidot = np.sin(phi_dot)
-        sthetadot = np.sin(theta_dot)
+        cphi = np.cos(phi)
+        ctheta = np.cos(theta)
 
         # Calculate tire forces
-        Ft1 = (
-            F10
-            + Kt * (zc1 - z1)
-            + Ct * (vzc1 - vz1)
-            + self.K_arf * stheta / (self.wf / 2)
-        )
-        Ft2 = (
-            F20
-            + Kt * (zc2 - z2)
-            + Ct * (vzc2 - vz2)
-            - self.K_arf * stheta / (self.wf / 2)
-        )
-        Ft3 = (
-            F30
-            + Kt * (zc3 - z3)
-            + Ct * (vzc3 - vz3)
-            + self.K_arr * stheta / (self.wf / 2)
-        )
-        Ft4 = (
-            F40
-            + Kt * (zc4 - z4)
-            + Ct * (vzc4 - vz4)
-            - self.K_arr * stheta / (self.wf / 2)
-        )
+        Ft1 = F10 + Kt * (zc1 - z1) + Ct * (vzc1 - vz1)
+        Ft2 = F20 + Kt * (zc2 - z2) + Ct * (vzc2 - vz2)
+        Ft3 = F30 + Kt * (zc3 - z3) + Ct * (vzc3 - vz3)
+        Ft4 = F40 + Kt * (zc4 - z4) + Ct * (vzc4 - vz4)
 
         # Calculate suspension forces
         Fs1 = (
             F10
-            + K_sf * (z1 - z - self.wf / 2 * sphi + self.lf * stheta)
-            + C_sf * (vz1 - vz - self.wf / 2 * sphidot + self.lf * sthetadot)
+            + self.K_sf * (z1 - z - self.wf / 2 * sphi + self.lf * stheta)
+            + self.C_sf
+            * (vz1 - vz - phi_dot * self.wf / 2 * cphi + theta_dot * self.lf * ctheta)
         )
         Fs2 = (
             F20
-            + K_sf * (z2 - z + self.wf / 2 * sphi + self.lf * stheta)
-            + C_sf * (vz2 - vz + self.wf / 2 * sphidot + self.lf * sthetadot)
+            + self.K_sf * (z2 - z + self.wf / 2 * sphi + self.lf * stheta)
+            + self.C_sf
+            * (vz2 - vz + phi_dot * self.wf / 2 * cphi + theta_dot * self.lf * ctheta)
         )
         Fs3 = (
             F30
-            + K_sr * (z3 - z - self.wr / 2 * sphi - self.lr * stheta)
-            + C_sr * (vz3 - vz - self.wr / 2 * sphidot - self.lr * sthetadot)
+            + self.K_sr * (z3 - z - self.wr / 2 * sphi - self.lr * stheta)
+            + self.C_sr
+            * (vz3 - vz - phi_dot * self.wr / 2 * cphi - theta_dot * self.lr * ctheta)
         )
         Fs4 = (
             F40
-            + K_sr * (z4 - z + self.wr / 2 * sphi - self.lr * stheta)
-            + C_sr * (vz4 - vz + self.wr / 2 * sphidot - self.lr * sthetadot)
+            + self.K_sr * (z4 - z + self.wr / 2 * sphi - self.lr * stheta)
+            + self.C_sr
+            * (vz4 - vz + phi_dot * self.wr / 2 * cphi - theta_dot * self.lr * ctheta)
         )
-        return Ft1, Ft2, Ft3, Ft4, Fs1, Fs2, Fs3, Fs4
+
+        # Calculate anti-roll bar forces
+        Farf = self.K_arf * (z2 - z1)  # The force is positive in 1 and negative in 2
+        Farr = self.K_arr * (z4 - z3)  # The force is positive in 3 and negative in 4
+
+        return Ft1, Ft2, Ft3, Ft4, Fs1, Fs2, Fs3, Fs4, Farf, Farr
 
     def get_steer_wbw(self, delta_sw):
         # return the ackerman geometry wheel by wheel